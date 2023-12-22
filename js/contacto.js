654fd777278ca50ae9ddf1e00e2fa11eaaa3b22d
Edgar Mila <edgarmila_10@outlook.com>
2023-12-22 19:12:15 +0100

    22-12-2023

---
+++ js/contacto.js
@@ -0,0 +1,6 @@
+//Cambiar los href por el nombre del dominio final.
+window.addEventListener("load", ()=>{
+    document.getElementById("logo").addEventListener("click", ()=>{
+        location.href="http://127.0.0.1:5500/index.html"
+    })
+})
